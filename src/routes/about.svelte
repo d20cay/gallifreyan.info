762d7c03749485451924425adf76f62a96be0862
d20cay <d20cayt@gmail.com>
2020-12-30 14:13:21 +0100

    Updates about me page to include information on publisher and goals

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -19,15 +19,8 @@
 </svelte:head>
 
 <h1>{PageName.get($currentPage)}</h1>
-<p>
-	My name is d20cay and I love Doctor Who, probably more than anything yet, well except my best
-	friends of course. Since I'm currently doing an apprenticeship as a software engineer, I thought
-	it would be a good idea to unify as many Gallifreyan guides as possible in a website. In my free
-	time, I like to program, watch Netflix and hang out with friends. Basically the typical nerd
-	hobbies.
-</p>
 
-<h2>How I got into Gallifreyan</h2>
+<h2>Author</h2>
 <div uk-grid>
 	<div class="uk-width-1-4@l uk-width-1-3@m uk-width-1-1@s">
 		<Image src="img/about/come_along_pond.svg"
@@ -35,6 +28,13 @@
 		       desc="Come along Pond"/>
 	</div>
 	<div class="uk-width-3-4@l uk-width-2-3@m uk-width-1-1@s">
+		<p>
+			Hi, my name is d20cay and I love Doctor Who, probably more than anything yet, well
+			except my best friends of course. Since I'm currently doing an apprenticeship as a
+			software engineer, I thought it would be a good idea to unify as many Gallifreyan guides
+			as possible in a website. In my free time, I like to program, watch Netflix and hang out
+			with friends. Basically the typical nerd hobbies.
+		</p>
 		<p>
 			I'm generally very interested in languages. I've already been learning Esperanto for a
 			year and started learning Japanese and Klingon from Star Trek a while ago. So as I one
@@ -47,4 +47,22 @@
 			in the community.
 		</p>
 	</div>
+
+	<h2>Content Provider & Publisher</h2>
+	<p>
+		All of the content on this page (<a href="https://gallifreyan.info">gallifreyan.info</a>) is
+		written and published by myself. I hope to get some help with the writing of guides soon
+		because I'm very busy but for now it's just me. The website and the server that all of this
+		runs on is operated by <a href="https://ovh.com">OVH</a>.
+	</p>
+
+	<h2>Goals</h2>
+	<p>
+		My main goal with this website is to spread the word about the amazing possibilities,
+		flexibility and versatility of Gallifreyan by providing comprehensive guides that anyone can
+		understand and apply. This was the goal when I started and will hopefully stay that way
+		until I'm unable to entertain this website by my financial means. <br>
+		Secondarily I also hope get enough traction through this site that I can use the commissions
+		that people send me as a serious side hustle.
+	</p>
 </div>
